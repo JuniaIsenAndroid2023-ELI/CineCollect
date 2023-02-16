1afb2e764f52da6f873fd71c6e878232f2d92d46
adenzu <57950444+adenzu@users.noreply.github.com>
2023-02-16 13:35:39 +0100

    added login direction to main page

--- app/src/main/java/com/example/cinecollect/MainActivity.java
+++ app/src/main/java/com/example/cinecollect/MainActivity.java
@@ -2,13 +2,37 @@ package com.example.cinecollect;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if (currentUser == null) {
+            // TODO
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        startActivity(new Intent(this, AuthenticationActivity.class));
     }
 }
