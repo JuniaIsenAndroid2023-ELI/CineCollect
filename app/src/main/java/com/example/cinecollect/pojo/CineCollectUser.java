26f6ca1ce56e006f8fafc3cd8dcf8b79f7db5d15
adenzu <57950444+adenzu@users.noreply.github.com>
2023-02-28 01:10:01 +0100

    created POJOs for database

---
+++ app/src/main/java/com/example/cinecollect/pojo/CineCollectUser.java
@@ -0,0 +1,76 @@
+package com.example.cinecollect.pojo;
+
+public class CineCollectUser {
+    public String username;
+    public String email;
+    public Boolean emailVerified = false;
+    public Boolean hasFilms = false;
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setEmailVerified(Boolean emailVerified) {
+        this.emailVerified = emailVerified;
+    }
+
+    public Boolean getEmailVerified() {
+        return emailVerified;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setHasFilms(Boolean hasFilms) {
+        this.hasFilms = hasFilms;
+    }
+
+    public Boolean getHasFilms() {
+        return hasFilms;
+    }
+
+    public static Builder newBuilder() {
+        return new Builder();
+    }
+
+    public static class Builder {
+        private final CineCollectUser user;
+
+        private Builder() {
+            this.user = new CineCollectUser();
+        }
+
+        public Builder setUsername(String username) {
+            user.setUsername(username);
+            return this;
+        }
+
+        public Builder setEmailVerified(Boolean emailVerified) {
+            user.setEmailVerified(emailVerified);
+            return this;
+        }
+
+        public Builder setEmail(String email) {
+            user.setEmail(email);
+            return this;
+        }
+
+        public Builder setHasFilms(Boolean hasFilms) {
+            user.setHasFilms(hasFilms);
+            return this;
+        }
+
+        public CineCollectUser build() {
+            return user;
+        }
+    }
+}
--- app/src/main/java/com/example/cinecollect/pojo/Film.java
+++ app/src/main/java/com/example/cinecollect/pojo/Film.java
@@ -5,4 +5,59 @@ public class Film {
     public String releaseDate;
     public String genre;
     public String director;
+
+    private Film() {
+    }
+
+    public static Builder newBuilder() {
+        return new Builder();
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setReleaseDate(String releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public void setDirector(String director) {
+        this.director = director;
+    }
+
+    public static class Builder {
+        private final Film film;
+
+        private Builder() {
+            this.film = new Film();
+        }
+
+        public Builder setTitle(String title) {
+            film.setTitle(title);
+            return this;
+        }
+
+        public Builder setReleaseDate(String releaseDate) {
+            film.setReleaseDate(releaseDate);
+            return this;
+        }
+
+        public Builder setGenre(String genre) {
+            film.setGenre(genre);
+            return this;
+        }
+
+        public Builder setDirector(String director) {
+            film.setDirector(director);
+            return this;
+        }
+
+        public Film build() {
+            return film;
+        }
+    }
 }
